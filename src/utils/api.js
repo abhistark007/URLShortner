a16683190bf39bd7c940a33c18ec62ce4450e508
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:02:41 +0530

    Update api.js
    Fixed the response that was getting from axios.get

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,18 @@
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { fetchShortURLResults } from "../store/shortURLSlice";
+
 
 
 const BASE_URL="https://api.shrtco.de/v2/";
-const dispatch=useDispatch();
+
 
 
 export const getShortenURL= async(url)=>{
-    axios.get(BASE_URL+url)
-    .then((res)=>{
-        dispatch(fetchShortURLResults(res));
-        console.log(res);
-    })
-    .catch((e)=>console.log(e));
+    try{
+        const data=await axios.get(BASE_URL+"shorten?url="+url);
+        // console.log("SSSS",data);
+        return data;
+    }catch(e){
+        console.log("ERRRRRRRRR",e);
+        return e;
+    }
 }
