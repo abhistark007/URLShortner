fd628d6b77a7b2c1d213a4b35b0f6ac119f79322
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:49:17 +0530

    Update App.jsx
    Added if else that if data===false then it means error occured so set our value to {}.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { getShortenURL } from './utils/api'
 import { useEffect } from 'react';
 import { fetchShortURLResults } from './store/shortURLSlice';
 import SearchBar from './components/SearchBar';
+import ShortUrlDisplayCard from './components/ShortUrlDisplayCard';
 
 
 function App() {
@@ -13,14 +14,20 @@ function App() {
 
   const getShortUrl=async(url)=>{
     const data=await getShortenURL(url);
+    // console.log(data);
+    if(data===false){
+      dispatch(fetchShortURLResults({}));  
+    }else{
     dispatch(fetchShortURLResults(data.data.result));
+    }
   } 
 
   return (
     <div className='flex flex-col bg-black min-h-screen items-center'>
         <div className='flex flex-col w-[60%]'>
             <SearchBar getShortUrl={getShortUrl}/>
-            <p className='text-white'>{value.short_link}</p>
+            {/* <p className='text-white'>{value.short_link}</p> */}
+            <ShortUrlDisplayCard url={value.short_link}/>
         </div>
     </div>
   )
