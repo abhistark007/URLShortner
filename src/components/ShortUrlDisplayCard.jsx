4091680eba1ee47b84daf9cd34e143409144f16d
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:48:44 +0530

    Create ShortUrlDisplayCard.jsx
    Used to display the shortURL generated

---
+++ src/components/ShortUrlDisplayCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {AiOutlineCopy} from 'react-icons/ai'
+
+function ShortUrlDisplayCard({url}) {
+  if(!url){
+    return (
+        <div className='hidden flex bg-purple-600 text-white rounded-lg items-center'>
+            <p>{url}</p>
+            <div className='flex justify-center items-center'>
+                <AiOutlineCopy/>
+            </div>
+        </div>
+      )
+  }
+
+  return (
+    <div className='flex bg-purple-600 text-white rounded-lg items-center'>
+        <p>{url}</p>
+        <div className='flex justify-center items-center'>
+            <AiOutlineCopy/>
+        </div>
+    </div>
+  )
+}
+
+export default ShortUrlDisplayCard
