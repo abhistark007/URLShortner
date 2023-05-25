020192163471c22fc5310c297b35fbc3aeea83a4
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 16:07:33 +0530

    Update ShortUrlDisplayCard.jsx

--- src/components/ShortUrlDisplayCard.jsx
+++ src/components/ShortUrlDisplayCard.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import {AiOutlineCopy} from 'react-icons/ai'
+import { toast } from 'react-toastify';
 
 function ShortUrlDisplayCard({url}) {
+
+
+    const handleCopy=()=>{
+        navigator.clipboard.writeText(url);
+        toast("Link Copied !")
+    }
+
   if(!url){
     return (
         <></>
@@ -9,9 +17,9 @@ function ShortUrlDisplayCard({url}) {
   }
 
   return (
-    <div className='flex bg-purple-600 text-white rounded-lg items-center'>
+    <div className='flex bg-purple-600 text-white rounded-lg items-center py-4 justify-between px-10 my-5'>
         <p>{url}</p>
-        <div className='flex justify-center items-center'>
+        <div onClick={handleCopy} className='hover:text-pink-600 duration-100 cursor-pointer flex justify-center items-center text-3xl'>
             <AiOutlineCopy/>
         </div>
     </div>
