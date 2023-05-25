fe3198b2d12f21ea74775945aac4399862d6550f
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:03:18 +0530

    Update App.jsx
    the use shortner is working and it is updating the state too

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { getShortenURL } from './utils/api'
+import { useEffect } from 'react';
+import { fetchShortURLResults } from './store/shortURLSlice';
+
 
 function App() {
+  const {value}=useSelector((state)=>state.shortURL);
+  const dispatch=useDispatch();
+  
+
+  const getShortUrl=async()=>{
+    const data=await getShortenURL("www.google.com");
+    dispatch(fetchShortURLResults(data.data.result));
+  } 
 
   return (
-    <div>
-      
+    <div className='flex flex-col'>
+        
     </div>
   )
 }
