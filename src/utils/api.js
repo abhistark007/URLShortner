d8c85c867854efa91c42b9b1ff86c59739113488
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 16:12:42 +0530

    Update api.js
    Added Link Generation success toast and Error occured error toast

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 
 
@@ -10,9 +11,11 @@ export const getShortenURL= async(url)=>{
     try{
         const data=await axios.get(BASE_URL+"shorten?url="+url);
         // console.log("SSSS",data);
+        toast("Link Generated Successfully");
         return data;
     }catch(e){
         // console.log(e.response.data.ok);
+        toast.error(e.response.data.error);
         return e.response.data.ok;
     }
 }
