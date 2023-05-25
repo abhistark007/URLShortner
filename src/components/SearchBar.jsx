94cb56064e6a17402335f0a42c9f160b7d61d0fc
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:37:00 +0530

    Create SearchBar.jsx
    A search bar with a button which onClick calls getShortUrl function

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+function SearchBar({getShortUrl}) {
+    const [url,setUrl]=useState("");
+
+    const handleClick=()=>{
+        if(url.length>0){
+            getShortUrl(url);
+        }
+    }
+  return (
+    <div className='flex border-2 border-purple-600 rounded-full'>
+        <input type='text' value={url} onChange={(e)=>setUrl(e.target.value)} placeholder='paste url to be shortened' className='bg-black text-white outline-none py-2 px-3 rounded-l-full flex-1'/>
+        <button onClick={handleClick} className='bg-purple-600 rounded-r-full px-3 text-white'>Generate</button>
+    </div>
+  )
+}
+
+export default SearchBar
