cf86ec582e3f6a56c91e5f154fe7272d67ee2a43
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:52:44 +0530

    Update ShortUrlDisplayCard.jsx
    Fixed both flex and hidden in class name as both are same and cant coexist

--- src/components/ShortUrlDisplayCard.jsx
+++ src/components/ShortUrlDisplayCard.jsx
@@ -4,7 +4,7 @@ import {AiOutlineCopy} from 'react-icons/ai'
 function ShortUrlDisplayCard({url}) {
   if(!url){
     return (
-        <div className='hidden flex bg-purple-600 text-white rounded-lg items-center'>
+        <div className='hidden  bg-purple-600 text-white rounded-lg items-center'>
             <p>{url}</p>
             <div className='flex justify-center items-center'>
                 <AiOutlineCopy/>
