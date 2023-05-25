ba5fbd3d5bada308e739335e1218d42c889b9941
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-25 15:37:14 +0530

    Update App.jsx
    Added SearchBar in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { getShortenURL } from './utils/api'
 import { useEffect } from 'react';
 import { fetchShortURLResults } from './store/shortURLSlice';
+import SearchBar from './components/SearchBar';
 
 
 function App() {
@@ -10,14 +11,17 @@ function App() {
   const dispatch=useDispatch();
   
 
-  const getShortUrl=async()=>{
-    const data=await getShortenURL("www.google.com");
+  const getShortUrl=async(url)=>{
+    const data=await getShortenURL(url);
     dispatch(fetchShortURLResults(data.data.result));
   } 
 
   return (
-    <div className='flex flex-col'>
-        
+    <div className='flex flex-col bg-black min-h-screen items-center'>
+        <div className='flex flex-col w-[60%]'>
+            <SearchBar getShortUrl={getShortUrl}/>
+            <p className='text-white'>{value.short_link}</p>
+        </div>
     </div>
   )
 }
